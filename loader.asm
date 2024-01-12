cb75a7810a808bafebefbd988fbceb8be09114b7
ACCISA <aniss.chalah@gmail.com>
2024-01-12 13:15:23 -0500

    interrupt handling

--- loader.asm
+++ loader.asm
@@ -2,22 +2,24 @@
 [ORG 0x7e00]
 
 start:
-    mov [DriveId], dl
-    mov eax, 0x80000000
+    mov [DriveId],dl
+
+    mov eax,0x80000000
     cpuid
-    cmp eax, 0x80000001
+    cmp eax,0x80000001
     jb NotSupport
-    mov eax, 0x80000001
-    cpuid 
-    test edx, (1<<29)
+
+    mov eax,0x80000001
+    cpuid
+    test edx,(1<<29)
     jz NotSupport
-    test edx, (1<<26)
+    test edx,(1<<26)
     jz NotSupport
 
 LoadKernel:
     mov si,ReadPacket
     mov word[si],0x10
-    mov word[si+2],100 ; load kb for our kernel
+    mov word[si+2],100
     mov word[si+4],0
     mov word[si+6],0x1000
     mov dword[si+8],6
@@ -28,57 +30,53 @@ LoadKernel:
     jc  ReadError
 
 GetMemInfoStart:
-    mov eax, 0xe820
-    mov edx, 0x534d4150
-    mov ecx, 20
-    mov edi, 0x9000
-    xor ebx, ebx
+    mov eax,0xe820
+    mov edx,0x534d4150
+    mov ecx,20
+    mov edi,0x9000
+    xor ebx,ebx
     int 0x15
     jc NotSupport
 
 GetMemInfo:
-    add edi, 20 ; each mem block is 20 bytes
-    mov eax, 0xe820
-    mov edx, 0x534d4150
-    mov ecx, 20
+    add edi,20
+    mov eax,0xe820
+    mov edx,0x534d4150
+    mov ecx,20
     int 0x15
     jc GetMemDone
 
-    test ebx, ebx
+    test ebx,ebx
     jnz GetMemInfo
 
 GetMemDone:
-
-
 TestA20:
-    mov ax, 0xffff
-    mov es, ax
-
+    mov ax,0xffff
+    mov es,ax
     mov word[ds:0x7c00],0xa200
     cmp word[es:0x7c10],0xa200
     jne SetA20LineDone
-    mov word[0x7c00], 0xb200
-    cmp word[es:0x7c10], 0xb200
+    mov word[0x7c00],0xb200
+    cmp word[es:0x7c10],0xb200
     je End
-
+    
 SetA20LineDone:
     xor ax,ax
     mov es,ax
 
 SetVideoMode:
-    mov ax, 3
+    mov ax,3
     int 0x10
-
+    
     cli
     lgdt [Gdt32Ptr]
     lidt [Idt32Ptr]
 
-    mov eax, cr0
+    mov eax,cr0
     or eax,1
-    mov cr0, eax
+    mov cr0,eax
 
     jmp 8:PMEntry
-
 
 ReadError:
 NotSupport:
@@ -95,16 +93,59 @@ PMEntry:
     mov ss,ax
     mov esp,0x7c00
 
-    mov byte[0xb8000],'P'
-    mov byte[0xb8001],0xa
+    cld
+    mov edi,0x70000
+    xor eax,eax
+    mov ecx,0x10000/4
+    rep stosd
+    
+    mov dword[0x70000],0x71007
+    mov dword[0x71000],10000111b
+
+    lgdt [Gdt64Ptr]
+
+    mov eax,cr4
+    or eax,(1<<5)
+    mov cr4,eax
+
+    mov eax,0x70000
+    mov cr3,eax
+
+    mov ecx,0xc0000080
+    rdmsr
+    or eax,(1<<8)
+    wrmsr
+
+    mov eax,cr0
+    or eax,(1<<31)
+    mov cr0,eax
+
+    jmp 8:LMEntry
 
 PEnd:
     hlt
     jmp PEnd
 
+[BITS 64]
+LMEntry:
+    mov rsp,0x7c00
 
-DriveId: db 0
+    cld
+    mov rdi,0x200000
+    mov rsi,0x10000
+    mov rcx,51200/8
+    rep movsq
 
+    jmp 0x200000 ; jump to the memory where the kernel asm is
+
+
+LEnd:
+    hlt
+    jmp LEnd
+    
+    
+
+DriveId:    db 0
 ReadPacket: times 16 db 0
 
 Gdt32:
@@ -124,9 +165,21 @@ Data32:
     db 0xcf
     db 0
     
-
 Gdt32Len: equ $-Gdt32
+
 Gdt32Ptr: dw Gdt32Len-1
           dd Gdt32
+
 Idt32Ptr: dw 0
           dd 0
+
+
+Gdt64:
+    dq 0
+    dq 0x0020980000000000
+
+Gdt64Len: equ $-Gdt64
+
+
+Gdt64Ptr: dw Gdt64Len-1
+          dd Gdt64
